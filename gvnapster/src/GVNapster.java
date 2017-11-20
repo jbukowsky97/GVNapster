b2fa38032055a694e53bde113beb4f711e14f4d1
Troy Madsen <madsentr@eos17.cis.gvsu.edu>
2017-11-19 19:38:22 -0500

    -Minor fixes to GVNapster.java

---
+++ gvnapster/src/GVNapster.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * The main class of the GVNapster client. Responsible for controlling the view and model of the program.
+ *
+ * @author Troy Madsen
+ */
+public class GVNapster {
+
+	/**
+	* Entry point of the GVNapster program.
+	*
+	* @param args The list of command line arguments to pass at run time.
+	*/	
+	public static void main(String[] args) {
+		
+	}
+
+}
