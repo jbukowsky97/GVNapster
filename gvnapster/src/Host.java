a7d79eba924b79807790409cc6d525207dfb9e32
Troy Madsen <madsentr@dc19.cis.gvsu.edu>
2017-11-20 20:41:07 -0500

    -GVNapster controls Host and GUI fully -Host functionality to interface registration server

--- gvnapster/src/Host.java
+++ gvnapster/src/Host.java
@@ -11,7 +11,10 @@ public class Host {
 	private ClientHost client;
 
 	/** The server of the peer. */
-	private ServerHost server;	
+	private ServerHost server;
+
+	/** Search data */
+	private String[][] searchData;
 
 	/**
 	* Constructs a new Host object.
@@ -23,6 +26,15 @@ public class Host {
 		// Create a new server
 		server = new ServerHost();
 		server.start();
+	}
+
+	/**
+	* Provides the search data of the model.
+	*
+	* @return A two-dimensional String array of the search results from the registration server
+	*/
+	public String[][] getSearchData() {
+		return searchData;
 	}
 
 	/**
@@ -49,6 +61,36 @@ public class Host {
 	*/
 	public void quit() {
 		client.command("quit");
+	}
+
+	/**
+	* Register with the registration server.
+	*
+	* @param serverHostname Host address of the registration server
+	* @param port Port of registration server to connect to
+	* @param username Username of the client
+	* @param hostname Host address of the client
+	* @param speed Speed of the peer connect
+	*/
+	public void register(String serverHostname, int port, String username, String hostname, String speed) {
+		//TODO Sam register with the registration server
+		//TODO Send my file list to registration server
+	}
+
+	/**
+	* Query the registratiopn server
+	*
+	* @param search Search query to send to registration server
+	*/
+	public void search(String search) {
+		//TODO Sam pass search query on to registration server
+	}
+
+	/**
+	* Disconnect from the registration server.
+	*/
+	public void disconnect() {
+		//TODO Sam disconnect from the registration server
 	}
 
 }
